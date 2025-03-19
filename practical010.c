3556ad2f1bff3f12e2dbeb2c5ef13414847b9de8
dhruvi-gvp <240130510041.cs@gujaratvidyapith.org>
2025-03-19 09:36:08 +0530

    Add files via upload

---
+++ practical010.c
@@ -0,0 +1,12 @@
+//write a program to call a function that send message of your number.
+#include<stdio.h>
+void message()
+{
+    printf("5362587872");
+}
+void main()
+{
+    printf("my number is: ");
+    message();
+}
+
