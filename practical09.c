93ce77cbf6b90b447bc0b11a16be2e2771e97b54
dhruvi-gvp <240130510041.cs@gujaratvidyapith.org>
2025-03-19 09:35:50 +0530

    Add files via upload

---
+++ practical09.c
@@ -0,0 +1,11 @@
+//Write a program to call a function that send message of your name.
+#include <stdio.h>
+void message()
+{
+    printf("Deny patel.");
+}
+void main()
+{
+    printf("My name is: ");
+    message();
+}
